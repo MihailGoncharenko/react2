fff4cd7697b7c346fd674a850a870171849fabed
MihailGoncharenko <30863259+MihailGoncharenko@users.noreply.github.com>
2022-05-23 23:43:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+class ShopItemClass extends React.Component {
+	staticpropTypes = {
+		item:PropTypes.object.isRequired
+	}
+	constructor(props)
+	{
+		super(props);
+	}
+	render () {
+    return (
+	  <div class="main-content">
+	  <h2>{item.brand}</h2>
+	  <h1>{item.title}</h1>
+	  <h3>{item.description}</h3>
+	  <div class="description">
+		{item.descriptionFull}
+	  </div>
+	  <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+	  <div class="divider"></div>
+	  <div class="purchase-info">
+		<div class="price">{item.currency}{item.price}</div>
+		<button>Добавить в корзину</button>
+	  </div>
+	  </div>
+	);
+	}
+}
+
+
+function App() {
+  return (
+  <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemClass item={item} />
+    </div>
+  </div>
+	);
+}
+
+export default App;
